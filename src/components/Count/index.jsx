243e62eec5db06923f9545d0bc4886d687f48c7b
freya wang <yujuewa@gmail.com>
2020-12-30 14:25:37 -0800

    mini redux implement

--- src/components/Count/index.jsx
+++ src/components/Count/index.jsx
@@ -1,39 +1,43 @@
 import React, { Component } from 'react'
-
+import store from '../../redux/store'
 export default class Count extends Component {
-    state = {count: 0}
+    //state = {count: 0}
+
+    /*componentDidMount() {
+        store.subscribe(() => {
+            this.setState({})
+        })
+    }*/
+    
 
     increment = () => {
         const {value} = this.selectNumber
-        const {count} = this.state
-        this.setState({count: count + value * 1})
+        store.dispatch({type:'increment', data: value * 1})
     } 
     decrement = () => {
         const {value} = this.selectNumber
-        const {count} = this.state
-        this.setState({count: count - value * 1})
+        store.dispatch({type:'decrement', data: value * 1})
 
     }
     incrementIfOdd = () => {
         const {value} = this.selectNumber
-        const {count} = this.state
-        if (count % 2 !== 2) {
-            this.setState({count: count + value * 1})
+        const count = this.state
+        if (count % 2 !== 0) {
+            store.dispatch({type:'increment', data: value * 1})
         }
         
     }
     incrementAsync = () => {
         const {value} = this.selectNumber
-        const {count} = this.state
         setTimeout(() => {
-            this.setState({count: count + value * 1})
+            store.dispatch({type:'increment', data: value * 1})
         }, 500)
         
     }
     render() {
         return (
             <div>
-                <h1>current sum is: {this.state.count}</h1>
+                <h1>current sum is: {store.getState()}</h1>
                 <select ref= {c => this.selectNumber = c}>
                     <option value ="1">1</option>
                     <option value ="2">2</option>
---
+++ src/redux/count_reducer.js
@@ -0,0 +1,15 @@
+const initState = 0
+export default function countReducer(preState = initState, action) {
+    // if (preState === undefined) {
+    //     preState = 0
+    // } 
+    const {type, data} = action
+    switch(type) {
+        case 'increment':
+            return preState + data
+        case 'decrement':
+            return preState - data
+        default:
+            return preState
+    }
+}
